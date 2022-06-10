782cfbc7b62671c0ad40b6b7e7a43d45caa90f3c
Alok Jha <jalok302@gmail.com>
2022-06-10 23:12:05 +0530

    Adding files

---
+++ fpage.js
@@ -0,0 +1,5 @@
+function donow(){
+    var name = document.getElementById('now').value 
+    var newName = name;   document.getElementById('changeb').innerHTML = "Happy Birthday" + " "+ newName ;
+    document.getElementById('aloka').style.display = "none";
+}
